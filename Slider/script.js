7cf56525b37f0e72b25119be99e240357912ec4e
ChiaviniK <125836394+ChiaviniK@users.noreply.github.com>
2025-05-21 11:10:57 -0300

    Delete Slider directory

--- Slider/script.js
+++
@@ -1,9 +0,0 @@
-const imagemGrande = document.querySelector("#imagem-grande");
-const miniaturas = document.querySelectorAll("#miniaturas-container img");
-
-miniaturas.forEach(miniatura => {
-    miniatura.addEventListener('click', function() {
-        const caminhoImagemGrande = this.dataset.fullImage;
-        imagemGrande.src = caminhoImagemGrande;
-    })
-})
